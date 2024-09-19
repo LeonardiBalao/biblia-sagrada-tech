3c5b0caaeec584af3e544465037fb8f133cd86c8
Balao <matheus.balao@hotmail.com>
2024-09-19 20:24:13 -0300

    Fix change frequency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,7 +22,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       removeChapterNumbers(verse.chapter.name)
     )}/${verse.chapter.slug}?versiculo=${verse.number}`,
     lastModified: new Date().toISOString(),
-    changefreq: "monthly",
+    changeFrequency: "monthly",
     priority: 0.6,
   }));
 }
